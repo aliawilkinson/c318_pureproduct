8d3fafdcf6cbf74f136b2306c8c520e955ed3433
Alia Wilkinson <aliawilkinson@gmail.com>
2018-08-30 18:49:42 -0700

    backend updates

---
+++ server/db_connect.php
@@ -0,0 +1,3 @@
+<?php
+$db = mysqli_connect('hanhpham.live', 'pure_product_user', 'sephorasucks','pure_product',3306);
+?>
---
+++ server/migration_file/migration_connect.php
@@ -0,0 +1,3 @@
+<?php
+$db = mysqli_connect('hanhpham.live', 'harrisonChen', '1234','pure_product',3306);
+?>
